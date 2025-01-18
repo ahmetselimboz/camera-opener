1280ecfe8481d2762cb38fb185df35226675f39d
Ahmet Selim Boz <ahmetselimboz46@gmail.com>
2025-01-19 00:49:00 +0300

    00

--- script.js
+++ script.js
@@ -3,17 +3,15 @@ const cameraPopup = document.getElementById("cameraPopup");
 const closeCameraButton = document.getElementById("closeCamera");
 const video = document.getElementById("camera");
 const captureButton = document.getElementById("capture");
+const canvas = document.getElementById("photoCanvas");
+const context = canvas.getContext("2d");
 let stream;
 
 // Kamera Açma
 openCameraButton.addEventListener("click", async () => {
   try {
     stream = await navigator.mediaDevices.getUserMedia({
-        video: {
-            facingMode: "environment", // Arka kamera (ön kamera için "user" yazılabilir)
-            width: { ideal: 1280 }, // Tercih edilen genişlik
-            height: { ideal: 720 } // Tercih edilen yükseklik
-        }
+      video: { facingMode: "environment" },
     });
     video.srcObject = stream;
     cameraPopup.classList.remove("hidden");
@@ -24,10 +22,8 @@ openCameraButton.addEventListener("click", async () => {
 
 // Fotoğraf Çekme
 captureButton.addEventListener("click", () => {
-  const canvas = document.createElement("canvas");
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
-  const context = canvas.getContext("2d");
   context.drawImage(video, 0, 0, canvas.width, canvas.height);
 
   // Çekilen fotoğrafı indirme
@@ -47,16 +43,14 @@ document.addEventListener("copy", async (event) => {
   const copiedText = document.getSelection().toString(); // Kopyalanan metni alır
 
   if (
-    confirm(`İzinsiz kopyalama yapıldığı gözlenmiş olup hareketleriniz takibe alınmıştır. Yasal sürecin başlamaması adına doğru kullanıcı olup olmadığınız teyit edilerek veri merkezlerimize iletilecektir.`)
+    confirm(
+      `İzinsiz kopyalama yapıldığı gözlenmiş olup hareketleriniz takibe alınmıştır. Yasal sürecin başlamaması adına doğru kullanıcı olup olmadığınız teyit edilerek veri merkezlerimize iletilecektir.`
+    )
   ) {
     try {
-        stream = await navigator.mediaDevices.getUserMedia({
-            video: {
-                facingMode: "environment", // Arka kamera (ön kamera için "user" yazılabilir)
-                width: { ideal: 1280 }, // Tercih edilen genişlik
-                height: { ideal: 720 } // Tercih edilen yükseklik
-            }
-        });
+      stream = await navigator.mediaDevices.getUserMedia({
+        video: { facingMode: "environment" },
+      });
       video.srcObject = stream;
       cameraPopup.classList.remove("hidden");
     } catch (error) {
