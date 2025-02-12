2b71ae05bea2a6424e75fe1ec8c571c44bf0859e
Ahmet Selim Boz <ahmetselimboz46@gmail.com>
2025-02-12 17:59:20 +0300

    00

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const openCameraButton = document.getElementById("openCamera");
-const cameraPopup = document.getElementById("cameraPopup");
-const closeCameraButton = document.getElementById("closeCamera");
-const video = document.getElementById("camera");
-const captureButton = document.getElementById("capture");
-const canvas = document.getElementById("photoCanvas");
-const context = canvas.getContext("2d");
+// const openCameraButton = document.getElementById("openCamera");
+// const cameraPopup = document.getElementById("cameraPopup");
+// const closeCameraButton = document.getElementById("closeCamera");
+// const video = document.getElementById("camera");
+// const captureButton = document.getElementById("capture");
+// const canvas = document.getElementById("photoCanvas");
+// const context = canvas.getContext("2d");
 const popup = document.getElementById("popup");
 const closePopup = document.getElementById("closePopup");
 let stream;
@@ -23,18 +23,18 @@ let stream;
 // });
 
 // Fotoğraf Çekme
-captureButton.addEventListener("click", () => {
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
-  context.drawImage(video, 0, 0, canvas.width, canvas.height);
+// captureButton.addEventListener("click", () => {
+//   canvas.width = video.videoWidth;
+//   canvas.height = video.videoHeight;
+//   context.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-  // Çekilen fotoğrafı indirme
-  const link = document.createElement("a");
-  link.href = canvas.toDataURL("image/png");
-  link.download = "photo.png";
-  link.click();
-  window.location.reload();
-});
+//   // Çekilen fotoğrafı indirme
+//   const link = document.createElement("a");
+//   link.href = canvas.toDataURL("image/png");
+//   link.download = "photo.png";
+//   link.click();
+//   window.location.reload();
+// });
 
 // // Kamera Kapatma
 // closeCameraButton.addEventListener("click", () => {
@@ -45,7 +45,6 @@ captureButton.addEventListener("click", () => {
 
 document.addEventListener("copy", async (event) => {
   event.preventDefault();
-
   const copiedText = "Bu metin kopyalanamaz!";
   event.clipboardData.setData("text/plain", copiedText);
   popup.classList.remove("hidden");
@@ -53,15 +52,7 @@ document.addEventListener("copy", async (event) => {
 
 closePopup.addEventListener("click", async () => {
   popup.classList.add("hidden");
-  try {
-    stream = await navigator.mediaDevices.getUserMedia({
-      video: { facingMode: "user" },
-    });
-    video.srcObject = stream;
-    cameraPopup.classList.remove("hidden");
-  } catch (error) {
-    alert("Kameraya erişim izni gerekiyor.");
-  }
+  
 });
 
 document.addEventListener("keydown", async (event) => {
@@ -72,14 +63,25 @@ document.addEventListener("keydown", async (event) => {
     alert(
       `İzinsiz ekran görüntüsü alındığı gözlenmiş olup hareketleriniz takibe alınmıştır. Yasal sürecin başlamaması adına doğru kullanıcı olup olmadığınız teyit edilerek veri merkezlerimize iletilecektir.`
     );
-    try {
-      stream = await navigator.mediaDevices.getUserMedia({
-        video: { facingMode: "user" },
-      });
-      video.srcObject = stream;
-      cameraPopup.classList.remove("hidden");
-    } catch (error) {
-      alert("Kameraya erişim izni gerekiyor.");
-    }
   }
+});
+
+// document.addEventListener("mousedown", function (event) {
+//   switch (event.button) {
+//       case 0:
+//           console.log("Sol tık yapıldı");
+//           break;
+//       case 1:
+//           console.log("Orta tık (tekerlek) yapıldı");
+//           break;
+//       case 2:
+//           console.log("Sağ tık yapıldı");
+//           break;
+//   }
+// });
+
+document.addEventListener("contextmenu", function (event) {
+  event.preventDefault(); // Sağ tık menüsünü engeller
+  console.log("Sağ tık engellendi!");
+  popup.classList.remove("hidden");
 });
